5f762be1f50935edce27375685aee61090e5b010
Paul S <paul@xen.works>
2020-08-26 06:09:10 +0900

    first commit

---
+++ build/src/index.js
@@ -0,0 +1,24 @@
+"use strict";
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+Object.defineProperty(exports, "__esModule", { value: true });
+/*! THIS FILE IS AUTO-GENERATED */
+const googleapis_1 = require("./googleapis");
+exports.GoogleApis = googleapis_1.GoogleApis;
+const google = new googleapis_1.GoogleApis();
+exports.google = google;
+
+var v3_6 = require("./apis/identitytoolkit/v3");
+exports.identitytoolkit_v3 = v3_6.identitytoolkit_v3;
+//# sourceMappingURL=index.js.map
